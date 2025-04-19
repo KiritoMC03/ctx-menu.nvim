71262972bdeff0979fdb959cbcd8ea8dc6dfe2a5
Kirill Dudnik <kirill.dulim@gmail.com>
2025-04-19 14:37:30 +0200

    definitions

--- lua/ctx-menu/init.lua
+++ lua/ctx-menu/init.lua
@@ -2,19 +2,19 @@ local CtxMenu = {}
 
 --- @return boolean ok
 local function check_deps()
-	local ok, nui = pcall(require, "nui.popup")
+	local ok, _ = pcall(require, "nui.popup")
 	if not ok then
 		vim.notify("[CtxMenu] Missing dependency: 'nui'", vim.log.levels.ERROR)
 	end
 	return ok
 end
 
-function CtxMenu.init()
-	vim.notify("init")
-end
-
 function CtxMenu.show()
-	vim.notify("Hello")
+	if not check_deps() then
+		return
+	end
+
+	local Popup = require("nui.popup")
 end
 
 return CtxMenu
